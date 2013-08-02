9639895c9ea1011e80fc7a1c14651aecbf34e815
Lukman Hussein <lukmanism@gmail.com>
2013-08-02 12:15:45 +0800

    fresh copies

---
+++ helper.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ *
+ * @package   mod_formatic
+ * copyright ntahla.com/ Lukman Hussein
+ * @license GPL3
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
---
+++ mod_formatic.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ *
+ * @package   mod_formatic
+ * copyright Lukman Hussein
+ * @license GPL3
+ */
+
+// no direct access
+defined('_JEXEC') or die;
+
+require JModuleHelper::getLayoutPath('mod_formatic', $params->get('layout', 'default'));
+
